0a3dcb46a3dbaa7dcea90283ca7119dbbde28a53
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-04-27 16:56:35 -0300

    FEAT: implemente evento no botão de adicionar produtos ao carrinho e mostra os produtos adicionados

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { createTestScheduler } from 'jest';
 import { saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
@@ -12,25 +11,24 @@ const productsContainer = document.querySelector('.products');
 
 async function selectElements() {
   // const product = document.querySelectorAll('.product');
-  const cartList = document.querySelectorAll('.cart__products')
+  const cartList = document.querySelector('.cart__products');
   const cartBtns = document.querySelectorAll('.product__add');
-  cartBtns.forEach((button) =>
-    button.addEventListener('click', async () => {
-      // Com os dados do produtos retornados, você deve adicionar o produto ao carrinho de compras, utilize a função createCartProductElement (que se encontra no arquivo shopFunction.js) para criar os componentes HTML referentes a um produto do carrinho;
-      // Adicione o elemento retornado da função createCartElement(product) como filho do elemento <ol class="cart__products">.
-      const id = button.closest('.product').querySelector('.product__id').textContent;
-      // console.log(id);
-      saveCartID(id);
-      const showProductDetails = await (fetchProduct(id));
-      // console.log(showProductDetails);
-      const renderCartProducts = createCartProductElement({
-        id: showProductDetails.id,
-        title: showProductDetails.title,
-        price: showProductDetails.price,
-        pictures: showProductDetails.pictures
-      });
-      cartList.appendChild(showProductDetails)
-    }));
+  cartBtns.forEach((button) => button.addEventListener('click', async () => {
+    // Com os dados do produtos retornados, você deve adicionar o produto ao carrinho de compras, utilize a função createCartProductElement (que se encontra no arquivo shopFunction.js) para criar os componentes HTML referentes a um produto do carrinho;
+    // Adicione o elemento retornado da função createCartElement(product) como filho do elemento <ol class="cart__products">.
+    const id = button.closest('.product').querySelector('.product__id').textContent;
+    // console.log(id);
+    saveCartID(id);
+    const showProductDetails = await (fetchProduct(id));
+    // console.log(showProductDetails);
+    const renderCartProducts = createCartProductElement({
+      id: showProductDetails.id,
+      title: showProductDetails.title,
+      price: showProductDetails.price,
+      pictures: showProductDetails.pictures,
+    });
+    cartList.appendChild(renderCartProducts);
+  }));
 }
 
 async function renderProducts() {
@@ -86,8 +84,6 @@ async function fetchApi() {
     hideLoadingMessage();
   }
 }
-
-
 
 fetchApi();
 renderProducts();
